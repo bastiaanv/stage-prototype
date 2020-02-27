7d1e08dbd9080d98808a83af81c6a98a3c39c575
Bastiaan Verhaar <verhaar.bastiaan@gmail.com>
2020-02-27 12:10:55 +0100

    Improved documentation

--- src/main.ts
+++ src/main.ts
@@ -5,15 +5,21 @@ import { CyberPhysicalSystem } from './cps/cyber.physical.system.interface';
 import * as fs from 'fs';
 import * as util from 'util';
 
+// Reset log file
 fs.writeFileSync(__dirname + '/../debug.log', '');
 
+// remap console.log in order to write every console.log to a file and print to console
 console.log = (d: any) => {
     fs.appendFileSync(__dirname + '/../debug.log', util.format(d) + '\n')
     process.stdout.write(util.format(d) + '\n');
 };
 
+// Generate data, following a linear form
 const snapshots = DataGenerator.generateLinearData(96);
+
+// Generate a Cyber Physical System for the Reinforcement learning to train in
 const cps: CyberPhysicalSystem = RoomTemperatureApproach.make(snapshots, 20, 12, 40, 10);
 
+// Create the reinforcement learning model and train it
 const rl = new ReinforcementLearning(1, 150, 150, 2);
 rl.train(cps);
--- src/tensorflow/reinforcement.learning.ts
+++ src/tensorflow/reinforcement.learning.ts
@@ -12,32 +12,35 @@ export class ReinforcementLearning {
     private readonly weights3: Variable;
     private readonly bias3: Variable;
 
+    // The neural network
     private model(x: Tensor): Tensor {
         return x.matMul(this.weights1).add(this.bias1).relu().matMul(this.weights2).add(this.bias2).relu().matMul(this.weights3).add(this.bias3);
     }
 
+    // Use the model to get the index of the predicted action based on the given state
     private predict(x: Tensor): Tensor {
         return this.model(x).argMax();
     }
 
+    // Train the model using the 
     private trainModel(newQ: Tensor, input: Tensor): void {
-        const optimizer = train.sgd(0.1);
+        const optimizer = train.sgd(0.5);
         optimizer.minimize(() => {
             const loss = sum(square(sub(newQ, this.model(input)))) as Scalar;
-            // loss.print();
+            loss.print();
             return loss;
         });
     }
 
 /*
 Neural network model:
-                Input                   Hidden layer 1                  Output
-            (countInput neurons) ->   (countHiddenLayer1 neurons)  ->  (countOutput neuron, values between 0 and 1)
+        Input                   Hidden layer 1                  Hidden layer 2                      Output
+    (countInput neurons) ->   (countHiddenLayer1 neurons)  ->  (countHiddenLayer2 neurons)   ->  (countOutput neuron)
 
 This model is trained via a reinforcement learning algorithm and uses the relu activation function.
-The lose function is a sigmoid cross entropy method with the AdamOptimizer as our learning partner.
-The accuracy is the mean squared error.
+The lose function is the mean squared error method with the Gradient descent optimizer as our learning partner.
 */
+
     constructor(countInput: number, countHiddenLayer1: number, countHiddenLayer2: number, countOutput: number) {
         this.weights1 = variable(randomNormal([countInput, countHiddenLayer1]));
         this.bias1 = variable(randomNormal([countHiddenLayer1]));
@@ -48,8 +51,13 @@ The accuracy is the mean squared error.
     }
 
     public async train(cpsCopy: CyberPhysicalSystem) {
+        // Discount
         const y = .99;
+
+        // Chance on random action
         let e = 0.1;
+
+        // Number of iterations
         const numEpisodes = 2000;
 
         for (let epoch = 0; epoch < numEpisodes; epoch++) {
@@ -58,10 +66,10 @@ The accuracy is the mean squared error.
 
             for (let batchNr = 0; batchNr < cps.datasetSize; batchNr++) {
                 // Get q values from Neural Network
-                const input = tensor([[cps.getCurrentTemp()]]);
+                const state = tensor([[cps.getCurrentTemp()]]);
                 const modelOutcome = await Promise.all([
-                    this.model(input).data(),
-                    this.predict(input).data()
+                    this.model(state).data(),
+                    this.predict(state).data()
                 ]);
                 const currentQ = modelOutcome[0];
                 const actions = modelOutcome[1];
@@ -74,7 +82,7 @@ The accuracy is the mean squared error.
                 // Take action
                 cps.step(this.actionToActionArray(actions[0], currentQ.length));
 
-                // Get and save reward
+                // Get and save reward (Facilicom coins) to Facilicom wallet
                 const coins: FacilicomCoin[] = cps.getReward();
                 wallet.add(coins);
 
@@ -85,7 +93,8 @@ The accuracy is the mean squared error.
                 const maxNewQ = Math.max(...this.float32ArrayToArray(newQ));
                 currentQ[actions[0]] = wallet.getLastValue() + y * maxNewQ;
 
-                this.trainModel(tensor(currentQ), input);
+                // Train the model based on new Q values and current state
+                this.trainModel(tensor(currentQ), state);
             }
 
             // Decrease chance on a random action as we progress in learning
@@ -94,6 +103,7 @@ The accuracy is the mean squared error.
         }
     }
 
+    // Transform a float32array to a basic js array
     private float32ArrayToArray(array: backend_util.TypedArray): number[] {
         const output: number[] = [];
         for (const item of array) {
@@ -103,6 +113,7 @@ The accuracy is the mean squared error.
         return output;
     }
 
+    // Creates an array which can be accepted by the CPS to preform actions
     private actionToActionArray(index: number, length: number): number[] {
         const output: number[] = [];
         for (let i = 0; i < length; i++) {
