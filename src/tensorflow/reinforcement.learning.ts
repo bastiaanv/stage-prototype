e819d2f8974ab9f767648b951f1dcf9807415869
bastiaanv <bastiankpn7800@gmail.com>
2020-03-24 12:55:39 +0100

    Update learningRate

--- src/tensorflow/reinforcement.learning.ts
+++ src/tensorflow/reinforcement.learning.ts
@@ -15,7 +15,7 @@ export class ReinforcementLearning {
 
     // Hyper parameters
     private readonly discount = 0.6;
-    private readonly learningRate = 0.05;
+    private readonly learningRate = 0.1;
     private readonly numEpochs = 600;
 
     // Neural network
