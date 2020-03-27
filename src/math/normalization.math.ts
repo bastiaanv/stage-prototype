bbec064e11576d56ec7ff7136454daf12ddce60e
bastiaanv <bastiankpn7800@gmail.com>
2020-03-27 12:35:20 +0100

    Added comments

--- src/math/normalization.math.ts
+++ src/math/normalization.math.ts
@@ -1,8 +1,14 @@
+// Formula: N = (x - min) / (max - min)
+
 export class Normalization {
+    // Max = 40
+    // Min = -20
     public static temperature(temperature: number): number {
         return (temperature + 20) / 60;
     }
 
+    // Max = {parameter}
+    // Min = 0
     public static reward(reward: number, max: number): number {
         return reward / max;
     }
