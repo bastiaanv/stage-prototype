8b275ba7ec26ffc25f3666b95d0fce59a4646eb0
bastiaanv <bastiankpn7800@gmail.com>
2020-03-27 11:20:00 +0100

    Added reward system

--- src/tensorflow/reinforcement.learning.ts
+++ src/tensorflow/reinforcement.learning.ts
@@ -1,21 +1,21 @@
 import * as tf from '@tensorflow/tfjs-node-gpu';
 import { resolve } from 'path';
 import { Learning } from './learning.interface';
+import { TemperatureReward } from '../reward/temperature.reward';
 
 export class ReinforcementLearning implements Learning {
     private readonly pathToModel = 'file://' + resolve(__dirname, '..', '..', 'model');
 
+    private readonly tempReward = new TemperatureReward();
     private readonly model: tf.LayersModel;
-    private readonly nrOfActions: number;
+    private readonly nrOfActions: number = 3;
 
     constructor() {
         const input = tf.input({shape: [1]});
         const dense1 = tf.layers.dense({units: 10, activation: 'relu'}).apply(input);
         const dense2 = tf.layers.dense({units: 10, activation: 'relu'}).apply(dense1);
-        const dense3 = tf.layers.dense({units: 3, activation: 'softmax', name: 'output'}).apply(dense2) as tf.SymbolicTensor;
+        const dense3 = tf.layers.dense({units: this.nrOfActions, activation: 'softmax'}).apply(dense2) as tf.SymbolicTensor;
         this.model = tf.model({inputs: input, outputs: dense3});
-
-        this.nrOfActions = 3;
 
         this.model.compile({
             optimizer: tf.train.adam(),
@@ -51,7 +51,7 @@ export class ReinforcementLearning implements Learning {
 
             // Get reward for NN and update actual array
             const actual = await actualTensor.data();
-            actual[action] = this.getReward(temp, action);
+            actual[action] = this.tempReward.getReward((temp * 60 - 20), action);
 
             // Train NN
             const label = tf.tensor([actual]);
@@ -65,37 +65,6 @@ export class ReinforcementLearning implements Learning {
 
             // Degrease chance on random action
             epsilon = 1/( ( i/50 ) + 10 );
-        }
-    }
-
-    private getReward(temp: number, action: number): number {
-        const actualTemp = temp * 60 - 20;
-        // Below 16 degrees, positive reward for turning on the heater
-        if (actualTemp < 16) {
-            if (action === 1) {
-                return 1
-
-            } else {
-                return 0;
-            }
-
-        // Above 20 degrees, positive reward for turning on the AC
-        } else if (actualTemp > 20) {
-            if (action === 2) {
-                return 1
-
-            } else {
-                return 0;
-            }
-
-        // Between 16 and 20 degrees, positive reward for doing noting
-        } else {
-            if (action === 0) {
-                return 1
-
-            } else {
-                return 0;
-            }
         }
     }
 
