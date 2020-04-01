9fe920f26cd07178a94861e786751123bf99dc63
Bastiaan Verhaar <3987804+bastiaanv@users.noreply.github.com>
2020-04-01 20:40:10 +0200

    Delete temperature.reward.ts

--- src/reward/temperature.reward.ts
+++
@@ -1,9 +0,0 @@
-/*
-    Between 16 and 20 degrees, positive reward. Else negative reward
-*/
-
-export class TemperatureReward {
-    getReward(temp: number): number {
-        return temp > 16 && temp <= 20 ? 1 : 0;
-    }
-}
