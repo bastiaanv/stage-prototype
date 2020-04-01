cd24e53e3c9639d7ffc8f68b665336c170e3c08a
Bastiaan Verhaar <3987804+bastiaanv@users.noreply.github.com>
2020-04-01 16:23:51 +0200

    Update control.reward.ts

--- src/reward/control.reward.ts
+++ src/reward/control.reward.ts
@@ -4,13 +4,17 @@
     Between 16 and 20 degrees, positive reward for doing noting. Else negative reward
 */
 
-export class ControlReward {
-    getReward(temp:number, action: number): number {
-        if (temp < 16 && action === 1 ||
+export class RewardSystem {
+    getReward(temp:number, action: number, dateTime: Date): number {
+        if (dateTime.getHours() => 20 || dateTime.getHours() <= 7) {
+            return 1;
+        
+        } else if (temp < 16 && action === 1 ||
             temp > 20 && action === 2 ||
             temp >= 16 && temp <= 20 && action === 0) {
 
             return 1;
+
         } else {
             return 0;
         }
